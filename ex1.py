15684b2a54924bd7e03ec36ec721d9b2fdde9696
RASSI <leonardoali2008@gmail.com>
2026-06-21 21:10:12 -0400

    Testing functions

---
+++ ex1.py
@@ -0,0 +1,7 @@
+
+print("Testing basics")
+choose_name = input( "Type your name: ")
+choose_age = input("Type your age: ")
+print("Your name is: ", choose_name)
+print("Your age is: ", choose_age)
+
---
+++ pi.py
@@ -0,0 +1,4 @@
+
+pi = 3.14414132
+print(f"O valor de pi é {pi:.2f}".format(pi))
+print("O valor arredondado de pi é :", round(pi,2))
